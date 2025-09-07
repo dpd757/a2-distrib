7066933dcd0a7f6413bdc978ab6956bd2c2fe019
David Durham <david.p.durham@leidos.com>
2025-09-07 19:12:39 -0400

    Gradient and step size

--- optimization.py
+++ optimization.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 ### 
 # IMPLEMENT ME! REPLACE WITH YOUR ANSWER TO PART 1B
-OPTIMAL_STEP_SIZE = 1.0
+OPTIMAL_STEP_SIZE = 0.11 #1.0
 ###
 
 def _parse_args():
@@ -41,7 +41,11 @@ def quadratic_grad(x1, x2):
     :param x2: second coordinate
     :return: a one-dimensional numpy array containing two elements representing the gradient
     """
-    raise Exception("Implement me!")
+
+    grad1 = 2 * (x1-1)
+    grad2 = 16 * (x2-1)
+
+    return np.array([grad1, grad2])
 
 
 def sgd_test_quadratic(args):
